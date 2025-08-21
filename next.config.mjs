749dea267bc9bd37f14c5905f78c0ce2712ed52a
Jonathan Yau <jonathan_yau@outlook.com>
2025-08-21 15:49:38 -0400

    update: ESlint rules update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Disable specific rules during build
+    rules: {
+      'react/no-unescaped-entities': 'off'
+    }
+  }
+};
 
 export default nextConfig;
