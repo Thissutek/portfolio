ff4f6aa70ff11d5b9534769738e9fe1c59dd2d75
Jonathan Yau <jonathan_yau@outlook.com>
2025-08-21 16:22:31 -0400

    Removed Uneeded Files

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 "use client";
 import HeroSection from "../components/home/HeroSection";
 import PortfolioShowcase from "../components/home/PortfolioShowcase";
-import UniqueValueSection from "../components/home/UniqueValueSection";
 
 export default function Home() {
   return (
     <div className="relative">
       <HeroSection />
-      <UniqueValueSection />
+
       <PortfolioShowcase />
     </div>
   );
